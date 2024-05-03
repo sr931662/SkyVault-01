9badb264f698b323b98dc3fb78b03da9cdd5617a
sr931662 <83828392+sr931662@users.noreply.github.com>
2024-05-03 14:16:05 +0530

    Add files via upload

---
+++ public/css/script.js
@@ -0,0 +1,8 @@
+const title = document.querySelector('.title')
+
+document.addEventListener('scroll', function() {
+    let value = window.scrollY
+    // console.Log(value)
+    // title.style.marginTop = value + 'px'
+    console.log('scrolled!')
+})
